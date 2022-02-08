c75ac061227abead5fd6a99c68c9f65a34f2265f
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-08 09:51:34 -0300

    Update mega_sena.py

--- desafios_python/mega_sena.py
+++ desafios_python/mega_sena.py
@@ -6,23 +6,20 @@ numeros = list()
 #contador = 1
 print('gerando...')
 print()
-sleep(0.3)
+sleep(0.7)
 
 for contador in range(1, quantidade+1):
   numeros.clear()
   numero = 0
   while numero <6:
-   num = random.randint(1, 60)
+   num = random.randint(1,60)
    if num not in numeros:  
     numeros.append(num)
   else:
-   continue
-
-print('teste cod erro')  
-numero+=1  
+    continue
+  numero+=1  
 numeros.sort()
 sleep(0.3)
 print(f'jogo {contador}: {numeros}')
 print()
 contador+=1
-  
