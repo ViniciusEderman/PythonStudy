9719994b3121322cf3179fd3cb9387fab09b278b
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-08 09:52:19 -0300

    Update mega_sena.py

--- desafios_python/mega_sena.py
+++ desafios_python/mega_sena.py
@@ -1,25 +1,23 @@
 import random
 from time import sleep
-
 quantidade = int(input('quantos jogos você quer sortear?: '))
 numeros = list()
 #contador = 1
 print('gerando...')
 print()
 sleep(0.7)
-
 for contador in range(1, quantidade+1):
   numeros.clear()
   numero = 0
   while numero <6:
-   num = random.randint(1,60)
-   if num not in numeros:  
-    numeros.append(num)
-  else:
-    continue
-  numero+=1  
-numeros.sort()
-sleep(0.3)
-print(f'jogo {contador}: {numeros}')
-print()
-contador+=1
+    num = random.randint(1,60)
+    if num not in numeros:
+      numeros.append(num)
+    else:
+      continue
+    numero+=1  
+  numeros.sort()
+  sleep(0.3)
+  print(f'jogo {contador}: {numeros}')
+  print()
+  contador+=1
